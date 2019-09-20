afde0c7fb0e3b193e95f0f7fa923cf55b09a901c
Bert Van Nuffelen <bert.van.nuffelen@tenforce.com>
2019-09-20 13:05:46 +0000

    update codelist presentation rules

--- linkeddataparser2.js
+++ linkeddataparser2.js
@@ -867,6 +867,11 @@ function make_nj_class(element, grouped, aux ) {
                   } 
   		  return racc;
                }, value.extra["ap-codelist"] );
+
+	      if ( codelisturi != "" ) {
+		if ( scoped_range[0].range_uri != "http://www.w3.org/2004/02/skos/core#Concept" ) {
+		    console.log("WARNING: the range of property " + value.name + ": <" value["@id"] + "> is not skos:Concept")
+	      }};
               
               prop = {
                     uri: value["@id"],
