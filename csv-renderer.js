1ea2f71f0ecbb9ee409dd63b2bb5433292c4b852
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2020-04-03 15:13:32 +0200

    apply javascript style standards

--- csv-renderer.js
+++ csv-renderer.js
@@ -1,13 +1,12 @@
-const fs = require("fs");
-const jsonfile = require('jsonfile');
-const jsonld = require('jsonld');
-const ldParser = require('./linkeddataparser2');
-const camelCase = require('camelcase');
-const papaparse = require('papaparse');
+const fs = require('fs')
+const jsonfile = require('jsonfile')
+const jsonld = require('jsonld')
+const ldParser = require('./linkeddataparser2')
+const camelCase = require('camelcase')
+const papaparse = require('papaparse')
 
- 
-var program = require('commander');
- 
+var program = require('commander')
+
 program
   .version('0.8.0')
   .usage('node csv-renderer.js renders the content of a CSV file into a jsonld template')
@@ -18,42 +17,39 @@ program
   .option('-i, --input <path>', 'input file (a csv file)')
   .option('-o, --output <path>', 'output file (a json file)')
 
-program.on('--help', function(){
+program.on('--help', function () {
   console.log('')
-  console.log('Examples:');
-  console.log('  $ csv-renderer --help');
-  console.log('  $ csv-renderer -i <input> -o <output>');
-});
+  console.log('Examples:')
+  console.log('  $ csv-renderer --help')
+  console.log('  $ csv-renderer -i <input> -o <output>')
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
 
-var output = program.output || 'output.json';
+var output = program.output || 'output.json'
 var csvoptions = {
-	header: true,
-	skipEmptyLines: true,
-	complete: function(results) {
-		console.log("Finished:");
-	}
-	}
+  header: true,
+  skipEmptyLines: true,
+  complete: function (results) {
+    console.log('Finished:')
+  }
+}
 
+render_csv(program.template, program.input, output)
+console.log('done')
 
-render_csv(program.template, program.input, output);
-console.log('done');
+function render_csv (templatefile, csvfilename, output) {
+  console.log('start reading')
+  var template = fs.readFileSync(templatefile, 'utf-8')
+  var csvf = fs.readFileSync(csvfilename, 'utf-8')
+  var csv = papaparse.parse(csvf, csvoptions)
 
+  var pt = parse_template(template)
+  //  var ren = render_template(pt, {'ID':'een identifier', 'STRING' : 'een string waarde', 'BOOLEAN': 'true', 'VAL' : 'I do not know'});
+  var ren = render_template(pt, csv.data)
+  //  console.log(ren);
 
-
-function render_csv(templatefile, csvfilename, output) {
-  console.log('start reading');
-  var template = fs.readFileSync(templatefile, 'utf-8');
-  var csvf = fs.readFileSync(csvfilename, 'utf-8');
-  var csv = papaparse.parse(csvf, csvoptions);
-
-  var pt = parse_template(template);
-//  var ren = render_template(pt, {'ID':'een identifier', 'STRING' : 'een string waarde', 'BOOLEAN': 'true', 'VAL' : 'I do not know'});
-  var ren = render_template(pt, csv.data);
-//  console.log(ren);
-
-/*
+  /*
   jsonfile.writeFile(output, ren, function (err) {
 		if (err) {
 		   // Set the exit code if there's a problem so bash sees it
@@ -64,69 +60,64 @@ function render_csv(templatefile, csvfilename, output) {
 		});
 */
 
-  let writeStream = fs.createWriteStream(output);
+  const writeStream = fs.createWriteStream(output)
 
-write_data(writeStream, ren);	
+  write_data(writeStream, ren)
   writeStream.on('finish', () => {
-	      console.log('wrote all data to file');
-  });
+	      console.log('wrote all data to file')
+  })
 
-	// close the stream
-   writeStream.end();
-	
-	console.log('finished rendering to ' + output);
+  // close the stream
+  writeStream.end()
+
+  console.log('finished rendering to ' + output)
 };
 
+function parse_template (file) {
+  var parsed_template = {
+    pt_full: [],
+    pt_vars: []
+  }
 
+  var file1 = file.split('{{')
+  var file2 = []
+  for (i in file1) {
+	   file2 = file2.concat(file1[i].split('}}'))
+  };
+  parsed_template.pt_full = file2
 
-	
-
-
-function parse_template(file) {
-	var parsed_template = {
-		pt_full: [],
-		pt_vars :[]
-	};
-	
-	var file1 = file.split('{{');
-	var file2 = [];
-	for (i in file1) {
-	   file2 = file2.concat(file1[i].split('}}'));
-	};
-	parsed_template.pt_full = file2;
-	
-	return parsed_template
+  return parsed_template
 }
 
-function render_template(parsed_template, data){
-	var renderedData = [];
-	for (i in data) {
-	  renderedData[i] = render_template_single(parsed_template,data[i]);
-	}
-	return renderedData;
+function render_template (parsed_template, data) {
+  var renderedData = []
+  for (i in data) {
+	  renderedData[i] = render_template_single(parsed_template, data[i])
+  }
+  return renderedData
 };
 
-function render_template_single(parsed_template, data){
-	let render = '';
-	for (i in parsed_template.pt_full) {
-	  let reminder = i % 2;
+function render_template_single (parsed_template, data) {
+  let render = ''
+  for (i in parsed_template.pt_full) {
+	  const reminder = i % 2
 	  if (reminder == 0) {
-		render = render + parsed_template.pt_full[i];
+      render = render + parsed_template.pt_full[i]
 	  } else {
-		render = render + data[parsed_template.pt_full[i]];
+      render = render + data[parsed_template.pt_full[i]]
 	  }
-	}
-	return render;
+  }
+  return render
 }
 
-function write_data(stream, data){
-        stream.write("[");
-	for (i in data) {
-	  stream.write(data[i]);
-          stream.write(",")
-	}
-        stream.write("]");
-	return true;
+function write_data (stream, data) {
+  stream.write('[')
+  for (i in data) {
+	  stream.write(data[i])
+    stream.write(',')
+  }
+  stream.write(']')
+  return true
 };
 
 /*
@@ -140,23 +131,18 @@ const transformRow2Json = new Transform({
 });
 */
 
-function streamcsv(template, input, output) {
-
-   var out = Papa.parse(input, {
-	header: true,
-	skipEmptyLines: true,
-	step: function(row) {
-		console.log("Row:", row.data);
-		console.log(render_template_single(template, data));
-		console.log("--------");
-                
-	},
-	complete: function() {
-		console.log("All done!");
-	}
-});
-   console.log(out);
-
+function streamcsv (template, input, output) {
+  var out = Papa.parse(input, {
+    header: true,
+    skipEmptyLines: true,
+    step: function (row) {
+      console.log('Row:', row.data)
+      console.log(render_template_single(template, data))
+      console.log('--------')
+    },
+    complete: function () {
+      console.log('All done!')
+    }
+  })
+  console.log(out)
 }
-
-
--- exampletemplate-generator.js
+++ exampletemplate-generator.js
@@ -1,14 +1,13 @@
-const fs = require("fs");
-const jsonfile = require('jsonfile');
-const jsonld = require('jsonld');
-const nunjucks = require('nunjucks');
-const ldParser = require('./linkeddataparser2');
-const camelCase = require('camelcase');
-const papaparse = require('papaparse');
+const fs = require('fs')
+const jsonfile = require('jsonfile')
+const jsonld = require('jsonld')
+const nunjucks = require('nunjucks')
+const ldParser = require('./linkeddataparser2')
+const camelCase = require('camelcase')
+const papaparse = require('papaparse')
 
+var program = require('commander')
 
-var program = require('commander');
- 
 program
   .version('0.8.0')
   .usage('node exampletemplate-generator.js creates jsonld files per class')
@@ -19,202 +18,194 @@ program
   .option('-i, --input <path>', 'input file (a jsonld file)')
   .option('-o, --output [directory]', 'output directory for the json files')
 
-program.on('--help', function(){
+program.on('--help', function () {
   console.log('')
-  console.log('Examples:');
-  console.log('  $ exampletemplate-generator --help');
-  console.log('  $ exampletemplate-generator -i <input> -o <output>');
-});
+  console.log('Examples:')
+  console.log('  $ exampletemplate-generator --help')
+  console.log('  $ exampletemplate-generator -i <input> -o <output>')
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
 
-var output = program.output || '/exampletemplates/';
+var output = program.output || '/exampletemplates/'
 nunjucks.configure(output, {
-    autoescape: true
-});
+  autoescape: true
+})
 
-render_exampletemplate_from_json_ld_file(program.input, program.output);
-console.log('done');
+render_exampletemplate_from_json_ld_file(program.input, program.output)
+console.log('done')
 
-
-
-function render_exampletemplate_from_json_ld_file(filename, outputdirectory) {
-  console.log('start reading');
-  var obj = {};
+function render_exampletemplate_from_json_ld_file (filename, outputdirectory) {
+  console.log('start reading')
   jsonfile.readFile(filename)
-  .then(
-       function(obj) { 
-        console.log('start processing');
-        var promise = {};
-        var hostname = program.hostname;
-        promise = ldParser.parse_json_ld_file_to_exampletemplates(filename, hostname);
+    .then(
+      function (obj) {
+        console.log('start processing')
+        var promise = {}
+        var hostname = program.hostname
+        promise = ldParser.parse_json_ld_file_to_exampletemplates(filename, hostname)
 
+        promise.then(function (parsed_json) {
+          parsed_json.documentroot = program.documentpath
+          if (program.debug) {
+            jsonfile.writeFile(program.debug, parsed_json, function (err) {
+              if (err) {
+                process.exitCode = 1
+                console.error(err)
+                throw err
+              }
+            })
+          };
 
-        promise.then(function(parsed_json) {
-          parsed_json.documentroot = program.documentpath;
-          if (program.debug) { 
-		jsonfile.writeFile(program.debug, parsed_json, function (err) {
-		if (err) {
-		   process.exitCode = 1;
-                   console.error(err);
-                   throw err;
-                   }
-		})};
+          console.log('start writing class templates')
+          let classes = parsed_json.classes
+          let filenamei = ''
+          for (const i in classes) {
+            filenamei = outputdirectory + '/' + camelCase(classes[i].name.nl) + '.json'
+            jsonfile.writeFile(filenamei, make_exampletemplate(classes[i]), function (err) {
+              if (err) {
+                // Set the exit code if there's a problem so bash sees it
+                process.exitCode = 1
+                console.error(err)
+                throw err
+              }
+            })
+          }
+          console.log('start writing datatype templates')
+          classes = parsed_json.datatypes
+          for (const i in classes) {
+            filenamei = outputdirectory + '/' + camelCase(classes[i].name.nl) + '.json'
+            jsonfile.writeFile(filenamei, make_exampletemplate(classes[i]), function (err) {
+              if (err) {
+                // Set the exit code if there's a problem so bash sees it
+                process.exitCode = 1
+                console.error(err)
+                throw err
+              }
+            })
+          }
 
-          console.log('start writing class templates');
-	  classes = parsed_json.classes;
-	  for (i in classes) {
-		var filenamei = outputdirectory + '/' + camelCase(classes[i].name.nl) + '.json';
-		jsonfile.writeFile(filenamei, make_exampletemplate(classes[i]), function (err) {
-		if (err) {
-		   // Set the exit code if there's a problem so bash sees it
-		   process.exitCode = 1;
-                   console.error(err);
-                   throw err;
-                   }
-		});
-	  }
-          console.log('start writing datatype templates');
-	  classes = parsed_json.datatypes;
-	  for (i in classes) {
-		var filenamei = outputdirectory + '/' + camelCase(classes[i].name.nl) + '.json';
-		jsonfile.writeFile(filenamei, make_exampletemplate(classes[i]), function (err) {
-		if (err) {
-		   // Set the exit code if there's a problem so bash sees it
-		   process.exitCode = 1;
-                   console.error(err);
-                   throw err;
-                   }
-		});
-	  }
-
-          console.log('start writing class & datatype contextfiles');
-	  classes = parsed_json.classes.concat(parsed_json.datatypes);
-	  for (i in classes) {
-		var filenamei = outputdirectory + '/context/' + camelCase(classes[i].name.nl) + '.jsonld';
-		jsonfile.writeFile(filenamei, make_exampletemplate_context(classes[i]), function (err) {
-		if (err) {
-		   // Set the exit code if there's a problem so bash sees it
-		   process.exitCode = 1;
-                   console.error(err);
-                   throw err;
-                   }
-		});
-	  }
-          console.log('The files have been saved to ' + outputdirectory);
-
-          });
-         })
-	.catch(error => { console.error(error); process.exitCode = 1; } ) 
+          console.log('start writing class & datatype contextfiles')
+          classes = parsed_json.classes.concat(parsed_json.datatypes)
+          for (const i in classes) {
+            filenamei = outputdirectory + '/context/' + camelCase(classes[i].name.nl) + '.jsonld'
+            jsonfile.writeFile(filenamei, make_exampletemplate_context(classes[i]), function (err) {
+              if (err) {
+                // Set the exit code if there's a problem so bash sees it
+                process.exitCode = 1
+                console.error(err)
+                throw err
+              }
+            })
+          }
+          console.log('The files have been saved to ' + outputdirectory)
+        })
+      })
+    .catch(error => { console.error(error); process.exitCode = 1 })
 }
 
+function make_exampletemplate (cj_class_desc) {
+  var cj_class = {
+    '@context': program.contextbase + '/' + camelCase(cj_class_desc.name.nl) + '.jsonld',
+    '@type': cj_class_desc.uri,
+    '@id': '{{ID}}'
+  }
+  for (const p in cj_class_desc.properties) {
+    var rp = range_repr(cj_class_desc.properties[p].scopedrange)
+    // console.log(cj_class_desc.properties[p]);
+    // console.log(cj_class_desc.properties[p].name);
+    // console.log(cj_class_desc.properties[p].name.nl);
+    if ((cj_class_desc.properties[p].name.nl !== null) && (cj_class_desc.properties[p].name.nl !== '')) {
+      cj_class[camelCase(cj_class_desc.properties[p].name.nl)] = rp
+    }
+  }
 
-function make_exampletemplate(cj_class_desc) {
-	var cj_class = {
-	     '@context' : program.contextbase+'/'+camelCase(cj_class_desc.name.nl) + '.jsonld',
-             '@type' : cj_class_desc.uri,
-	     '@id' : '{{ID}}'
-	      };
-	for (p in cj_class_desc.properties) {
-		var rp = range_repr(cj_class_desc.properties[p].scopedrange);
-		//console.log(cj_class_desc.properties[p]);
-		// console.log(cj_class_desc.properties[p].name);
-		// console.log(cj_class_desc.properties[p].name.nl);
-		if ((cj_class_desc.properties[p].name.nl != null ) && (cj_class_desc.properties[p].name.nl != "")) {
-	 
-		cj_class[camelCase(cj_class_desc.properties[p].name.nl)] = rp ;
-			   }
-	}
-	
-	var pt = parse_template(JSON.stringify(cj_class));
-	var ren = render_template(pt, {'ID':'een identifier', 'STRING' : 'een string waarde', 'BOOLEAN': 'true', 'VAL' : 'I do not know'});
-	//console.log(ren);
-	return cj_class;
+  var pt = parse_template(JSON.stringify(cj_class))
+  var ren = render_template(pt, { ID: 'een identifier', STRING: 'een string waarde', BOOLEAN: 'true', VAL: 'I do not know' })
+  // console.log(ren);
+  return cj_class
 }
 
-function make_exampletemplate_context(cj_class_desc) {
-	var cj_class_context = {}
-        cj_class_context[camelCase(cj_class_desc.name.nl)] = cj_class_desc.uri;
+function make_exampletemplate_context (cj_class_desc) {
+  var cj_class_context = {}
+  cj_class_context[camelCase(cj_class_desc.name.nl)] = cj_class_desc.uri
 
-	for (p in cj_class_desc.properties) {
-		var rp = range_repr_context(cj_class_desc.properties[p].uri, cj_class_desc.properties[p].scopedrange);
-		if ((cj_class_desc.properties[p].name.nl != null ) && (cj_class_desc.properties[p].name.nl != "")) {
-		cj_class_context[camelCase(cj_class_desc.properties[p].name.nl)] = rp;
-		};
-	}
-	return cj_class_context;
+  for (const p in cj_class_desc.properties) {
+    var rp = range_repr_context(cj_class_desc.properties[p].uri, cj_class_desc.properties[p].scopedrange)
+    if ((cj_class_desc.properties[p].name.nl != null) && (cj_class_desc.properties[p].name.nl !== '')) {
+      cj_class_context[camelCase(cj_class_desc.properties[p].name.nl)] = rp
+    };
+  }
+  return cj_class_context
 }
 
+function range_repr (prop_range) {
+  var pru = ''
+  if ((prop_range[0] != null) && (prop_range[0].range_puri != null)) {
+    pru = prop_range[0].range_puri
+    if (pru === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString') {
+      return { nl: '{{STRING}}' }
+    };
 
-
-function range_repr(prop_range) {
-	var pru = "";
-	if ((prop_range[0] != null) && (prop_range[0].range_puri != null)) {
-	pru = prop_range[0].range_puri;
-        if ( pru == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString' ) {
-	   return pr_represention = { 'nl' : '{{STRING}}'}
-	};
-
-	var pr_represention = '{{VAL}}';
-	if ( pru.startsWith('http://www.w3.org/2001/XMLSchema#') ) {
-	   pr_represention = '{{' + pru.substring(33,pru.length).toUpperCase() + '}}';
-	}
-	return pr_represention;
-        } else {
-		return '{{ANY}}';
-        };
+    var pr_represention = '{{VAL}}'
+    if (pru.startsWith('http://www.w3.org/2001/XMLSchema#')) {
+      pr_represention = '{{' + pru.substring(33, pru.length).toUpperCase() + '}}'
+    }
+    return pr_represention
+  } else {
+    return '{{ANY}}'
+  };
 };
 
-function range_repr_context(prop, prop_range) {
-	if ((prop_range[0] != null) && (prop_range[0].range_puri != null)) {
-	var pru = prop_range[0].range_puri;
-        if ( pru == 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString' ) {
-	   return pr_represention = { 
-		"@container": [ "@language", "@set" ],
-        	"@id": prop,
-        	"@type": "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString"
-		};
-	};
+function range_repr_context (prop, prop_range) {
+  if ((prop_range[0] != null) && (prop_range[0].range_puri != null)) {
+    var pru = prop_range[0].range_puri
+    if (pru === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString') {
+      return {
+        '@container': ['@language', '@set'],
+        '@id': prop,
+        '@type': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString'
+      }
+    };
 
-	if ( pru.startsWith('http://www.w3.org/2001/XMLSchema#') ) {
-	   return pr_represention = { 
-        	"@id": prop,
-        	"@type": pru
-		};
-	};
-	
-	return prop;
-	} else {
-		return ""
-	};
+    if (pru.startsWith('http://www.w3.org/2001/XMLSchema#')) {
+      return {
+        '@id': prop,
+        '@type': pru
+      }
+    };
+
+    return prop
+  } else {
+    return ''
+  };
 };
 
+function parse_template (file) {
+  var parsed_template = {
+    pt_full: [],
+    pt_vars: []
+  }
 
-function parse_template(file) {
-	var parsed_template = {
-		pt_full: [],
-		pt_vars :[]
-	};
-	
-	var file1 =  file.split('{{');
-	var file2 = [];
-	for (i in file1) {
-	   file2 = file2.concat(file1[i].split('}}'));
-	};
-	parsed_template.pt_full = file2;
-	
-	return parsed_template
+  var file1 = file.split('{{')
+  var file2 = []
+  for (const i in file1) {
+    file2 = file2.concat(file1[i].split('}}'))
+  };
+  parsed_template.pt_full = file2
+
+  return parsed_template
 }
 
-function render_template(parsed_template, data){
-	let render = '';
-	for (i in parsed_template.pt_full) {
-	  let reminder = i % 2;
-	  if (reminder == 0) {
-		render = render + parsed_template.pt_full[i];
-	  } else {
-		render = render + data[parsed_template.pt_full[i]];
-	  }
-	}
-	return render;
+function render_template (parsed_template, data) {
+  let render = ''
+  for (const i in parsed_template.pt_full) {
+    const reminder = i % 2
+    if (reminder === 0) {
+      render = render + parsed_template.pt_full[i]
+    } else {
+      render = render + data[parsed_template.pt_full[i]]
+    }
+  }
+  return render
 }
