f30ff30cca74c928c2262fc96942d982d4c5b403
unknown <annemie.w@web.de>
2020-10-21 14:51:26 +0200

    updated html generator

--- html-generator2old.js
+++ html-generator2old.js
@@ -7,7 +7,7 @@ const ldParser = require('./linkeddataparser3');
 
 var program = require('commander');
 const { description } = require("commander");
-
+ 
 program
   .version('0.8.0')
   .usage('node html-generator.js creates html pages for jsonld files ')
@@ -19,13 +19,14 @@ program
   .option('-r, --documentpath <path>', 'the document path on which the html is published')
   .option('-x, --debug <path>', 'the intermediate json which will be used by the templaterenderer')
   .option('-m, --mainlanguage <languagecode>', 'the language to display(a languagecode string)')
-  .option('-i, --input <path>', 'input file that is merged from the jsonld and its appropriate translation json (a jsonld file)')
+  .option('-i, --input <path>', 'input file (a jsonld file)')
+  .option('-l, --languageinput <path>', 'language input file (a json file)')
   .option('-o, --output <path>', 'output file (the html file)')
   .option('-e, --tempdir [directory]', 'the directory for intermediate processing')
 
 
 
-program.on('--help', function () {
+program.on('--help', function(){
   console.log('')
   console.log('Examples:');
   console.log('  $ html-generator --help');
@@ -35,71 +36,160 @@ program.on('--help', function () {
 program.parse(process.argv);
 
 var templatedir = program.templatedir || '/app/views';
-nunjucks.configure(templatedir, {
-  autoescape: true
+nunjucks.configure(templatedir , {
+    autoescape: true
 });
 
-render_html_from_json_ld_file(program.style, program.template, program.input, program.output, program.mainlanguage);
-//render_html_from_json_ld_file('voc', 'voc2_en.j2', '../workbench/Drafts/ldmerged.jsonld', '../workbench/Drafts/outputnewhtml.html', 'en')
+//program.tempdir = '.\\'
+render_html_from_json_ld_file(program.style, program.template, program.input, program.languageinput, program.output, program.mainlanguage);
+//render_html_from_json_ld_file('voc', 'voc2_en.j2', '../Drafts/originalld.jsonld', '../Drafts/originallden.json', '../Drafts/outputhtml.html', 'en')
 console.log('done');
 
 
 
-function render_html_from_json_ld_file(target, template, filename, output_filename, language) {
+function render_html_from_json_ld_file(target, template, input_filename, input_language_file, output_filename, language) {
   console.log('start reading');
   var obj = {};
-  jsonfile.readFile(filename)
-    .then(
-      function (obj) {
-        console.log('start processing');
-        var promise = {};
-        var hostname = program.hostname;
+  jsonfile.readFile(input_filename)
+  .then(
+       function(obj) { 
+        jsonfile.readFile(input_language_file)
+        .then(
+          function (languageinput) {
+            console.log('start processing');
+            var promise = {};
+            var hostname = program.hostname;
+            var filename =  program.tempdir + "/tempjson.jsonld"
+            create_new_input_file(obj, languageinput, language).then((myJson) => {
+              jsonfile.writeFile(filename, myJson)
+              .then(res => {
+                console.log('Write mergefile complete');
 
-        const forceskos = program.forceskos ? true : false;
-        switch (target) {
-          case "voc":
-            promise = ldParser.parse_ontology_from_json_ld_file_voc(filename, hostname, language);
-            break;
-          case "ap":
-            promise = ldParser.parse_ontology_from_json_ld_file_all(filename, hostname, forceskos, language);
-            break;
-          case "oj":
-            promise = ldParser.parse_ontology_from_json_ld_file_oj(filename, hostname, forceskos, language);
-            break;
-          default:
-            console.log("unknown or not provided target for the html rendering");
-        };
+                const forceskos = program.forceskos ? true : false;
+                console.log(filename)
+                switch(target) {
+                    case "voc": 
+                        promise = ldParser.parse_ontology_from_json_ld_file_voc(filename, hostname, language);
+                        break;
+                    case "ap": 
+                        promise = ldParser.parse_ontology_from_json_ld_file_all(filename, hostname, forceskos, language);
+                        break;
+                    case "oj": 
+                        promise = ldParser.parse_ontology_from_json_ld_file_oj(filename, hostname, forceskos, language);
+                        break;
+                    default:
+                        console.log("unknown or not provided target for the html rendering");
+                };
 
-        promise.then(function (parsed_json) {
-          parsed_json.documentroot = program.documentpath;
-          if (program.debug) {
-            jsonfile.writeFile(program.debug, parsed_json, function (err) {
-              if (err) {
-                process.exitCode = 1;
-                console.error(err);
-                throw err;
-              }
-            })
-          };
-          var html = nunjucks.render(template, parsed_json);
+                promise.then(function(parsed_json) {
+                  parsed_json.documentroot = program.documentpath;
+                  if (program.debug) { 
+                    jsonfile.writeFile(program.debug, parsed_json, function (err) {
+                    if (err) {
+                      process.exitCode = 1;
+                                  console.error(err);
+                                  throw err;
+                                  }
+                    })};
+                  var html = nunjucks.render(template, parsed_json);
 
-          const data = new Uint8Array(Buffer.from(html));
+                  const data = new Uint8Array(Buffer.from(html));
 
-          console.log('start writing');
-          fs.writeFile(output_filename, data, (err) => {
-            if (err) {
-              // Set the exit code if there's a problem so bash sees it
-              process.exitCode = 1
-              throw err;
-            }
-            console.log('The file has been saved to ' + output_filename);
-          });
+                  console.log('start writing');
+                  fs.writeFile(output_filename, data, (err) => {
+                    if (err) {
+                    // Set the exit code if there's a problem so bash sees it
+                    process.exitCode = 1
+                        throw err;
+                    }
+                    console.log('The file has been saved to ' + output_filename);
+                    });
 
+                  })
+                  .catch(error => { console.error(error); process.exitCode = 1; } ) ;
+              })
+              .catch(error => { console.error(error); process.exitCode = 1 })
+            })        
         })
-          .catch(error => { console.error(error); process.exitCode = 1; });
+      .catch(error => { console.error(error); process.exitCode = 1 })
+    })
+	.catch(error => { console.error(error); process.exitCode = 1; } ) 
+}
 
-      })
-    .catch(error => { console.error(error); process.exitCode = 1; })
+async function create_new_input_file(input, languageinput, goallanguage) {
+  console.log('create merged file')
+  var myJson = new Object
+  myJson = input
+  myJson.classes = getMergedObjects(input.classes, languageinput.classes)
+  myJson.properties = getMergedObjects(input.properties, languageinput.properties)
+  if (!(myJson["translation"] === undefined)) {
+    for (var o = 0; o < myJson["translation"].length; o++) {
+      if (myJson["translation"][o]["language"] == goallanguage) {
+        myJson = mergeConfigValues(myJson, myJson["translation"][o])
+      }
+    }
+  }
+  return myJson
+}
+
+function mergeConfigValues (myJson, translationObject) {
+  for (let [key, value] of Object.entries(translationObject)) {
+    if (!(myJson[key] === undefined)) {
+      myJson[key] = value
+    }
+  }
+  return myJson
+}
+
+//Iterate through array of the input and get the equivalent class in the updated version through their ID,
+//Create new class-array for the updated objects
+function getMergedObjects (inputArray, languageinputArray) {
+  console.log('Checking array...')
+
+  var newArray = new Array()
+  for (var i = 0; i < languageinputArray.length; i++) {
+    var currObject = languageinputArray[i]
+    var elementToCompare = get_matching_object(currObject, inputArray)
+    if (elementToCompare != null) {
+      var newObject = merge_two_objects(elementToCompare, currObject)
+      newArray.push(newObject)
+    } // else the element will not be added to the new Array and therefore deleted
+  }
+  return newArray
+}
+
+//Iterate through properties of the input and get the equivalent property in the updated version through their ID,
+//Create new property-array for the updated properties
+function getMergedProperties (translatedJson, updatedJson) {
+  console.log('Checking properties...')
+
+  var propertyArray = new Array()
+  for (var m = 0; m < translatedJson.properties.length; m++) {
+    var input = translatedJson.properties[m]
+    var elementToCompare = get_matching_property(input, updatedJson)
+    if (elementToCompare != null) {
+      var newProperty = get_object(elementToCompare, currClass)
+      propertyArray.push(newProperty)
+    }  
+  }
+  return propertyArray
+}
+
+function merge_two_objects (elementToCompare, currObject) {
+  var newObject = elementToCompare
+  for (let [key, value] of Object.entries(currObject)) {
+    newObject[key] = value
+  }
+
+  return newObject
+}
+function get_matching_object (languageInputObject, inputArray) {
+  for (i = 0; i < inputArray.length; i++) {
+    if (inputArray[i]['@id'] == languageInputObject['@id']) {
+      return inputArray[i]
+    }
+  }
+  return null
 }
 
 //======================================================================================
@@ -108,7 +198,7 @@ function render_html_from_json_ld_file(target, template, filename, output_filena
  * identify duplicates by iterating over the list and comparing if the same term is
  * being used to identify multiple values
  */
-function identify_duplicates(properties) {
+function identify_duplicates (properties) {
   var acc = new Map()
   acc = properties.reduce(function (accumulator, currentValue, currentIndex, array) {
     return urireducer(accumulator, currentValue, currentIndex, array)
@@ -143,7 +233,7 @@ const capitalizeFirst = (s) => {
 
 // auxiliary function to convert to camelcase with dealing special cases
 // TODO: what are the guidelines for contextual scoping in the labels?
-function toCamelCase(str) {
+function toCamelCase (str) {
   str = camelCase(str)
   // console.log(str)
   str = str.replace(/\s\(source\)/g, '(source)').replace(/\s\(target\)/g, '(target)')
@@ -152,7 +242,7 @@ function toCamelCase(str) {
 };
 
 // map an entity prop to its term
-function map_identifier(prop) {
+function map_identifier (prop) {
   let identifier = ''
   if (program.useLabels === 'label') {
     if (prop.label && prop.label.nl) {
@@ -170,7 +260,7 @@ function map_identifier(prop) {
 };
 
 // create a map (term -> list of uri)
-function urireducer(accumulator, currentValue, currentIndex, array) {
+function urireducer (accumulator, currentValue, currentIndex, array) {
   let currentlist = []
   const term = map_identifier(currentValue)
   if (accumulator.has(term)) {
@@ -183,7 +273,7 @@ function urireducer(accumulator, currentValue, currentIndex, array) {
   return accumulator
 };
 
-function get_EAname(entities) {
+function get_EAname (entities) {
   let acc = new Map()
   acc = entities.reduce(function (accumulator, currentValue, currentIndex, array) {
     return EAname(accumulator, currentValue, currentIndex, array)
@@ -193,7 +283,7 @@ function get_EAname(entities) {
 }
 
 // create a map (EA-Name -> term)
-function EAname(accumulator, currentValue, currentIndex, array) {
+function EAname (accumulator, currentValue, currentIndex, array) {
   let currentlist = []
   const term = map_identifier(currentValue)
   const eaname = currentValue.extra['EA-Name']
@@ -243,7 +333,7 @@ function join_contexts (context, value, key, map) {
 };
 */
 
-function make_context(classes, properties, externals, externalproperties) {
+function make_context (classes, properties, externals, externalproperties) {
   console.log('make context')
 
   var context = new Map()
@@ -264,21 +354,21 @@ function make_context(classes, properties, externals, externalproperties) {
    a context file per applicationprofile per class
    This requires knowledge in the input about the class hierarchy
 */
-function map_class(c) {
+function map_class (c) {
   const mapping = new Map()
   const identifier = map_identifier(c)
   mapping.set(capitalizeFirst(identifier), c['@id'])
   return mapping
 };
 
-function classes(json) {
+function classes (json) {
   const classes = json.classes
   let classmapping = new Map()
   classmapping = classes.map(x => map_class(x))
   return classmapping
 }
 
-function map_properties(eanamesclasses, duplicates, prop) {
+function map_properties (eanamesclasses, duplicates, prop) {
   var mapping = new Map()
 
   var range
@@ -296,7 +386,7 @@ function map_properties(eanamesclasses, duplicates, prop) {
       range = prop.range[0]
     };
     range_uri = range.uri
-  };
+  } ;
   let atType = ''
   if (prop['@type'] === 'http://www.w3.org/2002/07/owl#ObjectProperty') {
     atType = '@id'
@@ -347,7 +437,7 @@ function map_properties(eanamesclasses, duplicates, prop) {
   return mapping
 }
 
-function properties(eanamesclasses, duplicates, json) {
+function properties (eanamesclasses, duplicates, json) {
   var props = json.properties
 
   var propertymapping = new Map()
@@ -356,14 +446,14 @@ function properties(eanamesclasses, duplicates, json) {
   return propertymapping
 }
 
-function map_external(c) {
+function map_external (c) {
   const mapping = new Map()
   const identifier = map_identifier(c)
   mapping.set(capitalizeFirst(identifier), c['@id'])
   return mapping
 };
 
-function externals(json) {
+function externals (json) {
   const externs = json.externals
 
   let externalmapping = new Map()
@@ -372,7 +462,7 @@ function externals(json) {
   return externalmapping
 }
 
-function externalproperties(eanamesclasses, duplicates, json) {
+function externalproperties (eanamesclasses, duplicates, json) {
   var externs = json.externalproperties
 
   var externalmapping = new Map()
